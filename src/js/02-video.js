f1143e1c84fafcff2522d1a623ea5e5f043daced
Yevhen <leshchenkoyevhen@gmail.com>
2022-09-21 17:25:15 +0300

    completed task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
 
+const onPlay = function (data) {
+  const timeOfWatch = data.seconds;
+  console.log(timeOfWatch);
+
+  localStorage.setItem('videoplayer-current-time', timeOfWatch);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+messageOut();
+
+function messageOut() {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
